705603cbd2a251fbb613932b35c16e934526b5ac
Karimuzzaman <pialkarimuzzaman@gmail.com>
2022-10-11 12:10:50 +0600

    1st answer added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -6,13 +6,13 @@ function AllCollapseExample() {
             <Accordion.Item eventKey="0">
                 <Accordion.Header>What is the purpose of react router?</Accordion.Header>
                 <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
+                    React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL. <br />
+                    React Router is an API for React applications.React Router uses dynamic routing.
+                    When we say dynamic routing, we mean routing that takes place as our app is rendering, not in a configuration or convention outside of a running app. That means almost everything is a component in React Router. <br />
+                    React Router, and dynamic, client-side routing, allows us to build a single-page web application with navigation without the page refreshing as the user navigates. React Router uses component structure to call components, which display the appropriate information.
+
+                    By preventing a page refresh, and using Router or Link, which is explained in more depth below, the flash of a white screen or blank page is prevented. This is one increasingly common way of having a more seamless user experience. React router also allows the user to utilize browser functionality like the back button and the refresh page while maintaining the correct view of the application.
+
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="1">
