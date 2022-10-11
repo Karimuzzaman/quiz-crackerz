5f313d6c6e41db9f9b072c00e10f8d3f52708b38
Karimuzzaman <pialkarimuzzaman@gmail.com>
2022-10-11 11:00:53 +0600

    home image and description added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import './Home.css'
+import Img from '../../images/quiz.jpg'
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is home</h1>
-
+        <div className="card mb-3 home container mt-5">
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src={Img} className="img-fluid rounded-start" alt="..." />
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">Quiz Ocean Mania</h5>
+                        <p className="card-text"><b>
+                            What does the ocean have to do with the air you breathe? Test your knowledge and impress your friends with this pop quiz on web development — and prepare to be surprised.</b></p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -4,6 +4,7 @@ const Topics = () => {
     return (
         <div>
             <h1>This is topics</h1>
+            <img src="../../images/quiz-1.jpg" alt="" />
 
         </div>
     );
