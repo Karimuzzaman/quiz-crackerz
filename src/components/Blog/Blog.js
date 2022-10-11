c8d17bdb23b9507855a03a08e5c581eae307e1f8
Karimuzzaman <pialkarimuzzaman@gmail.com>
2022-10-11 12:13:09 +0600

    2nd question added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -16,7 +16,7 @@ function AllCollapseExample() {
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="1">
-                <Accordion.Header>Accordion Item #2</Accordion.Header>
+                <Accordion.Header>How does Context API works?</Accordion.Header>
                 <Accordion.Body>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                     eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
