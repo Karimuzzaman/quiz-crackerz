ae972a630ce848c634a044581c0d0b594a8a46e1
Karimuzzaman <pialkarimuzzaman@gmail.com>
2022-10-11 10:22:42 +0600

    header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className='container d-md-flex d-sm-inline-flex justify-content-between bg-secondary p-3'>
+            <div>
+                <h4>Quiz Ocean Mania</h4>
+            </div>
+            <div className='header'>
+                <Link to="/">Home</Link>
+                <Link to="/topics">Topics</Link>
+                <Link to="/statistics">Statistics</Link>
+                <Link to="/blog">Blog</Link>
+
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
