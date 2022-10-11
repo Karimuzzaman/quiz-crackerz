e9eccd6efcd18a370c083f5a39c443c4caa1d281
Karimuzzaman <pialkarimuzzaman@gmail.com>
2022-10-11 15:12:33 +0600

    statistics added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import './Statistics.css'
+import { useLoaderData } from 'react-router-dom';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
-    return (
-        <div>
-            <h1>This is statistics</h1>
+    const { data } = useLoaderData();
 
-        </div>
+
+    return (
+        <LineChart className='container mt-5'
+            width={350}
+            height={400}
+            data={data}
+            margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5
+            }}
+        >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="total" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line
+                type="monotone"
+                dataKey="total"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+            />
+
+        </LineChart>
+
+
+
+
     );
 };
 
